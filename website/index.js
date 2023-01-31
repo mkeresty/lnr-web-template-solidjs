94c34369bf8961a2a4ecdf1eb97494fba297773b
derp <derp@derpfinance.com>
2023-01-30 20:36:31 -0500

    did quite a bit of refactoring. working toward a library, now able to load assets that have embedded links to blockchain assets

--- website/index.js
+++ website/index.js
@@ -1,1 +1,14 @@
-let derp = 1;
+let og = window.parent.og;
+
+async function derp(){
+  let walletAddress = await og.signer.getAddress();
+  console.log("log from chain: Wallet:" + walletAddress);
+  console.log(og);
+  console.log("UTF8 stuff 小馬, 昨夜のコンサートは最高でした جیم ذال sdf Д д Ж ж Ñ");
+  $("#test").html("Clone - jq good- ethers: good<br>" + walletAddress)
+}
+
+
+$(document).ready(function(){
+  derp();
+});
