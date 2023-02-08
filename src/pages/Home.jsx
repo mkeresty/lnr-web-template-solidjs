a3a94dc722cc3408b004bcf61d7eb8ccf95f9fb0
mkjuice <masonk@udel.edu>
2023-02-07 22:49:28 -0500

    included subgraph:)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,26 @@
 import styles from '../App.module.css';
 import * as THREE from 'three';
-import { createSignal, onMount, Show } from 'solid-js';
-import { nameLookup, handleEthers, getWrappedNames, getUnwrappedNames } from '../utils/nameUtils';
+import { createSignal, onMount, Show, For } from 'solid-js';
+import { nameLookup, handleEthers, getWrappedNames, getUnwrappedNames, getAllNames } from '../utils/nameUtils';
 
 const Home = () => {
+  var og = window.parent.og;
+  const [names, setNames] = createSignal('');
+  const [namesCount, setNamesCount] = createSignal(0);
 
-  const testfun =async () =>{
-    await getUnwrappedNames();
+  const getNames =async () =>{
+    var walletAddress = await og.signer.getAddress();
+    var test = "0x59657D79Fb2c0fa2C38C576f08866632Cf7729a6"
+    var repNames = await getAllNames(test);
+    setNames(repNames);
+    setNamesCount(repNames.length);
+    console.log(repNames)
   }
+
+  const testfunwrapped =async () =>{
+    await getWrappedNames();
+  }
+
 
 
     return(
@@ -21,9 +34,27 @@ const Home = () => {
           <p class="title has-text-light">
           home.og
           </p>
-          <button onClick={testfun}>test</button>
+          
           <br/>
           </div>
+          <button class="button is-outlined m-3" onClick={getNames}>get names</button>
+          <br />
+          {namesCount}
+          <br/>
+  
+          <For each={names()}>{(item, i) =>
+  
+          <div class="box is-ancestor">
+                  <div class="tile  m-1">
+                    <p class="title is-4">{item.name}</p>
+                    <br />
+                    <p class="subtitle is-6">{item.status}</p>
+                  </div>
+            </div>
+       
+      }</For>
+      
+
       </div>
       </div>
       </div>
--- src/utils/nameUtils.js
+++ src/utils/nameUtils.js
@@ -43,26 +43,19 @@ export async function handleEthers(fn){
 
 export async function getAllNames(nameAddress){
     var og = window.parent.og;
-    var tempAddress = "0x00C3670F155f2a0CA2b68882666CbA19d14e4943";
     var address = await resolveOrReturn(nameAddress)
     if(address == false){
         return
     }
-
-
-
+    var unwrapped = await getUnwrappedNames(address);
+    var wrapped = await getWrappedNames(address);
     
-    try{
-        var name = await og.lnr.lookupAddress(address);
-    } catch(error){
-        console.log(error)
-    }
-    return(name);
+    return(unwrapped.concat(wrapped))
+
 }
 
-export async function getWrappedNames(){
+export async function getWrappedNames(address){
     var og = window.parent.og;
-    var tempAddress = "0x00C3670F155f2a0CA2b68882666CbA19d14e4943";
     var ethers = window.parent.og.ethers;
     const provider = new ethers.providers.InfuraProvider(
         "mainnet",
@@ -71,17 +64,22 @@ export async function getWrappedNames(){
     const wrapperAddress = "0x2Cc8342d7c8BFf5A213eb2cdE39DE9a59b3461A7";
     const abi = [{"inputs":[{"internalType":"string","name":"_uri","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"pairId","type":"uint256"},{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":false,"internalType":"bytes32","name":"namer","type":"bytes32"}],"name":"Unwrapped","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"pairId","type":"uint256"},{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":false,"internalType":"bytes32","name":"namer","type":"bytes32"}],"name":"Wrapped","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"changeProxyAvail","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"changeWrapEnabled","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_name","type":"bytes32"}],"name":"createWrapper","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"nameId","type":"bytes32"}],"name":"getNameOwner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"idToName","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"nameBytes","outputs":[{"internalType":"contract Linagee","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"name":"nameToId","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"},{"internalType":"address","name":"_param2","type":"address"},{"internalType":"bool","name":"_param3","type":"bool"}],"name":"proxySetAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"},{"internalType":"bytes32","name":"_hash","type":"bytes32"}],"name":"proxySetContent","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"},{"internalType":"address","name":"_param2","type":"address"}],"name":"proxySetSubRegistrar","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_uri","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"unwrap","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"name":"waitForWrap","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_name","type":"bytes32"}],"name":"wrap","outputs":[],"stateMutability":"nonpayable","type":"function"}]
     const contract = new ethers.Contract(wrapperAddress, abi, provider)
-    const balance = (await contract.balanceOf(tempAddress)).toString();
+    const balance = (await contract.balanceOf(address)).toString();
     if(balance > 0){
         const tokenids =[];
         for(let i = 0; i < balance; i++){
-            const curId = (await contract.tokenOfOwnerByIndex(tempAddress, i)).toString();
+            const curId = (await contract.tokenOfOwnerByIndex(address, i)).toString();
             const curBytes = (await contract.idToName(curId)).toString();
             const curName = og.lnr.bytes32ToString(curBytes);
-            const isValid = og.lnr.isValidDomain(curName+'.og');
+            var isValid = false;
+            try{
+                var isValid = og.lnr.isValidDomain(curName.toString()+'.og'); //curName.toString()+'.og';
+            }
+            catch(e){
+                console.log(e)
+            }
             tokenids.push({bytes: curBytes, name: curName+'.og', isValid: isValid[0], tokenId: curId, status: "wrapped"});
         }
-        console.log(tokenids)
         return(tokenids)
     }
     return
@@ -89,33 +87,26 @@ export async function getWrappedNames(){
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
-export async function getUnwrappedNames(){
+export async function getUnwrappedNames(address){
     var og = window.parent.og;
-    var thegraph = "https://api.studio.thegraph.com/query/42000/linagee/v0.0.1"
-    var tempAddress = "0x00C3670F155f2a0CA2b68882666CbA19d14e4943";
-    var myaddress = "0xD0B12c1123a83d85ddf1F1ff23B5aF3FCDbF6799"
-    var other = "0x7e1877D6eD0574181E5508952CFCD057B5AC5832";
-    var ethers = window.parent.og.ethers;
-    const provider = new ethers.providers.InfuraProvider(
-        "mainnet",
-        "5b26585dfc17437da190dd2117648295"
-      );
-    var tokens = await theGraph(other);
+
+    var tokens = await theGraph(address);
     if(tokens && tokens['data']['domains']){
         const gData = tokens['data']['domains'];
         const tokenids =[];
         for(let i = 0; i < gData.length; i++){
-            console.log(i)
             const curId = undefined;
-            const curBytes = (gData[i]).domainBytecode
-            console.log(curBytes)
-            const curName = og.lnr.bytes32ToString(curBytes);
-            console.log(curName)
-            const isValid = og.lnr.isValidDomain(curName.toString()+'.og');
-            console.log(isValid)
+            const curBytes = ((gData[i]).domainBytecode).toString()
+            const curName = (og.lnr.bytes32ToString(curBytes)).toString();
+            var isValid = false;
+            try{
+                var isValid = og.lnr.isValidDomain(curName.toString()+'.og'); //curName.toString()+'.og';
+            }
+            catch(e){
+                console.log(e)
+            }
             tokenids.push({bytes: curBytes, name: curName+'.og', isValid: isValid[0], tokenId: curId, status: "unwrapped"});
         }
-        console.log(tokenids)
         return(tokenids)
     }
 
